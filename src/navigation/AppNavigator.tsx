013328005e5fefb1be3bca17af1db33ca31b0331
Jonatan Armando Martínez Javier <jona012311@gmail.com>
2020-09-24 23:10:26 -0600

    feat: change transitions between screens

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Animated, Easing } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '@app/screens/Home';
@@ -10,7 +11,18 @@ const Stack = createStackNavigator();
 function AppNavigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator headerMode={false}>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false,
+                    cardStyle: { backgroundColor: 'transparent' },
+                    cardStyleInterpolator: ({ current: { progress } }) => ({
+                        cardStyle: {
+                            opacity: progress
+                        },
+                    }),
+                }}
+                mode="modal"
+            >
                 <Stack.Screen name='Home' component={Home} />
                 <Stack.Screen name='Detail' component={Detail} />
                 <Stack.Screen name='MangaDetail' component={MangaDetail} />
