497862e18ddce25791d8d634d5c677955cab113d
Jonatan Armando Martínez Javier <jona012311@gmail.com>
2020-09-25 07:25:33 -0600

    feat: add style constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,7 @@
+export const HEADER_COLOR = '#003666';
+
+export const BUTTONS_COLOR = '#003666';
+export const BUTTONS_UNDERLAYER_COLOR = '#ff4c4c';
+
+export const TAG_COLOR = '#003666';
+export const TAG_TEXT_COLOR = "#ffffff";
---
+++ src/constants/dimensions.ts
@@ -0,0 +1,5 @@
+//Header
+export const HEADER_HEIGHT = 65;
+export const HEADER_ICON_SIZE = 30
+
+export const PADDING = 20;
